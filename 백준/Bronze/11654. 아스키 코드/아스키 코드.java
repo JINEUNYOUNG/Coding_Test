09060d29abba022a6c0e9384a4438e8c3c22d0ee
JINEUNYOUNG <131138113+JINEUNYOUNG@users.noreply.github.com>
2023-12-15 15:07:00 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.nextLine();
+		char c = s.charAt(0);
+		int num = (int)c;
+		System.out.println(num);
+		}
+		
+	}
