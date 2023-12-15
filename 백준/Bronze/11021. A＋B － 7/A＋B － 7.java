528052874eaa8f413b4f2458c6ba2a93a6372695
JINEUNYOUNG <131138113+JINEUNYOUNG@users.noreply.github.com>
2023-12-15 15:05:41 +0900

    [Bronze V] Title: A+B - 7, Time: 252 ms, Memory: 18640 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		
+		for (int i=1;i<=x;i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+		System.out.println("Case #"+i+": "+(a+b));}
+		
+	}
+	
+	
+	
+}
